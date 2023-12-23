bd36dd1009f5534b296f9e91fc0762efc6c34342
Egor2090 <egorvlasov504@gmail.com>
2023-12-23 11:02:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides(index) {
+  const slides = document.getElementsByClassName("slide");
+  
+  if (index >= slides.length) {
+    slideIndex = 0;
+  }
+  if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex].style.display = "block";
+}
+
+document.querySelector(".prev-slide").addEventListener("click", () => {
+  showSlides(--slideIndex);
+});
+
+document.querySelector(".next-slide").addEventListener("click", () => {
+  showSlides(++slideIndex);
+});
